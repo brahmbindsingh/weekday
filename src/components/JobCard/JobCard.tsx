10b63a27a288ea7b09870f0c7aec8463da568199
Brahm Bind Singh <57853873+brahmbindsingh@users.noreply.github.com>
2024-05-02 11:44:23 +0530

    job card styling done

--- src/components/JobCard/JobCard.tsx
+++ src/components/JobCard/JobCard.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import hourglass from '../../assets/images/hourglass.png';
 import { IJobDetails } from "../../constants/JobInterfaces";
+import { capitalizeString, getJobProfile } from "../../utils/utils";
 import './JobCard.css';
 
 const JobCard = (props: IJobDetails) => {
+
   return (
     <div className="job-card">
       <div className="post-date">
@@ -11,25 +13,25 @@ const JobCard = (props: IJobDetails) => {
         <p className="post-time">Posted 10 days ago</p>
       </div>
       <div className="company-details">
-        <img src="" alt="" className="company-logo" />
+        <img src="" alt="fampay" className="company-logo" />
         <div className="job-info">
-          <p className="company-name"></p>
-          <p className="job-profile"></p>
-          <p className="job-location"></p>
+          <p className="company-name">Fampay</p>
+          <p className="job-profile">{getJobProfile(props.jobRole)}</p>
+          <p className="job-location">{capitalizeString(props.location)}</p>
         </div>
       </div>
-      <div className="salary">
         {
           props.minJdSalary !== null ?
-            <p>Estimated Salary: <span>{props.minJdSalary}</span> - <span>{props.maxJdSalary}</span> LPA</p> :
-            <p>Estimated Salary: <span>{props.maxJdSalary}</span> LPA</p>
+            <p className="salary">Estimated Salary: <span>{props.minJdSalary}</span> - <span>{props.maxJdSalary}</span> LPA</p> :
+            <p className="salary">Estimated Salary: <span>{props.maxJdSalary}</span> LPA</p>
         }
-      </div>
-      <h3>About Company:</h3>
-      <h4>About Us</h4>
+      <h3 className="about-company">About Company:</h3>
+      <h4 className="about-us">About Us</h4>
       <div className="job-description">
         <p>{props.jobDetailsFromCompany}</p>
-        <div className="view-job">View Job</div>
+        <a href={props.jdLink}>
+          <div className="view-job">View Job</div>
+        </a>
       </div>
       <div className="experience">
         <p>Minimum Experience</p>
@@ -38,7 +40,10 @@ const JobCard = (props: IJobDetails) => {
       <div className="job-action-buttons">
         <button className="easy-apply">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 60 75" x="0px" y="0px"><path className = "svg-golden" d="M14.042,34H28a1,1,0,0,1,.978,1.209L24.151,57.749h0a.178.178,0,0,0,.116.227.174.174,0,0,0,.245-.064L46.793,27.656A1.038,1.038,0,0,0,45.958,26H32a1,1,0,0,1-.978-1.21L35.849,2.25a.178.178,0,0,0-.116-.227.24.24,0,0,0-.1-.024.18.18,0,0,0-.146.088L13.207,32.345A1.038,1.038,0,0,0,14.042,34Z"/></svg>
-          <p>EASY APPLY</p>
+          <p>Easy Apply</p>
+        </button>
+        <button className="ask-for-referral">
+          <p>Unlock referral asks</p>
         </button>
       </div>
     </div>
---
+++ src/utils/utils.ts
@@ -0,0 +1,19 @@
+export const getJobProfile = (job: string) => {
+    switch(job.toLowerCase()){
+        case 'backend':
+        case 'frontend':
+            return capitalizeString(job+" Engineer");
+    }
+}
+
+export const toCaptialCase = (value: string) => {
+    let str = value.trim().split('');
+    str[0] = str[0].toUpperCase();
+    return str.join('');
+}
+
+export const capitalizeString = (value: string) => {
+    let str = value.trim().split(' ');
+    str = str.map((el)=>toCaptialCase(el));
+    return str.join(' '); 
+}
